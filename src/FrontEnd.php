1ae27086782662d49fe2b8442234711a38fcc041
Caleb Mazalevskis <maikuolan@gmail.com>
2022-03-02 01:10:03 +0800

    Slightly restyle warnings.

--- src/FrontEnd.php
+++ src/FrontEnd.php
@@ -322,16 +322,19 @@ class FrontEnd
 
         /** Warns if maintenance mode is enabled. */
         if ($this->Loader->Configuration['core']['maintenance_mode']) {
-            $FE['Warnings'][] = '<span class="txtRd"><u>' . $this->Loader->L10N->getString('state_maintenance_mode') . '</u></span>';
+            $FE['Warnings'][] = $this->Loader->L10N->getString('state_maintenance_mode');
         }
 
         /** Warns if no signature files are active. */
         if (empty($this->Loader->Configuration['signatures']['active'])) {
-            $FE['Warnings'][] = '<span class="txtRd"><u>' . $this->Loader->L10N->getString('warning_signatures_1') . '</u></span>';
+            $FE['Warnings'][] = $this->Loader->L10N->getString('warning_signatures_1');
         }
 
         /** Prepare warnings. */
-        $FE['Warnings'] = count($FE['Warnings']) ? "\n<div class=\"center\">⚠️ " . implode(" ⚠️<br />\n⚠️ ", $FE['Warnings']) . ' ⚠️</div><hr />' : '';
+        $FE['Warnings'] = count($FE['Warnings']) ? "\n<div class=\"center\"><span class=\"warning\">" . implode(
+            "</span><br />\n<span class=\"warning\">",
+            $FE['Warnings']
+        ) . '</span></div><hr />' : '';
 
         /** Menu toggle JavaScript, needed by some front-end pages. */
         $MenuToggle =
