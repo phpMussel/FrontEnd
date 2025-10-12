66ccc19e245023512be7a37f9b721e5cecbb6ac1
Caleb Mazalevskis <maikuolan@gmail.com>
2025-10-12 15:03:24 +0800

    Aesthetic patch.

--- src/pages/accounts.php
+++ src/pages/accounts.php
@@ -8,27 +8,13 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: The accounts page (last modified: 2023.12.13).
+ * This file: The accounts page (last modified: 2025.10.11).
  */
 
 namespace phpMussel\FrontEnd;
 
 if (!isset($Page) || $Page !== 'accounts' || $this->Permissions !== 1) {
     die;
-}
-
-/** $_POST overrides for mobile display. */
-if (!empty($_POST['username']) && !empty($_POST['do_mob']) && (!empty($_POST['password_mob']) || $_POST['do_mob'] === 'delete-account')) {
-    $_POST['do'] = $_POST['do_mob'];
-}
-if (empty($_POST['username']) && !empty($_POST['username_mob'])) {
-    $_POST['username'] = $_POST['username_mob'];
-}
-if (empty($_POST['permissions']) && !empty($_POST['permissions_mob'])) {
-    $_POST['permissions'] = $_POST['permissions_mob'];
-}
-if (empty($_POST['password']) && !empty($_POST['password_mob'])) {
-    $_POST['password'] = $_POST['password_mob'];
 }
 
 /** A form has been submitted. */
@@ -92,8 +78,11 @@ if (!$FE['ASYNC']) {
         '},a=function(){%4$s\'%1$s\')};window.username=%2$s(e).value,window.passw' .
         'ord=%2$s(d).value,window.do=%2$s(t).value,\'delete-account\'==window.do&' .
         '&$(\'POST\',\'\',[%3$s,\'username\',\'password\',\'do\'],a,function(e){%' .
-        '4$se),hideid(i)},o),\'update-password\'==window.do&&$(\'POST\',\'\',[%3$' .
-        's,\'username\',\'password\',\'do\'],a,o,o)}' . "\n",
+        '4$se),document.getElementById(\'q1\'+i).classList.add(\'fmDelete\'),docu' .
+        'ment.getElementById(\'q2\'+i).classList.add(\'fmDelete\'),document.getEl' .
+        'ementById(\'q3\'+i).classList.add(\'fmDelete\'),document.getElementById(' .
+        '\'q4\'+i).classList.add(\'fmDelete\')},o),\'update-password\'==window.do' .
+        '&&$(\'POST\',\'\',[%3$s,\'username\',\'password\',\'do\'],a,o,o)}' . "\n",
         $this->Loader->L10N->getString('label.Loading_'),
         'document.getElementById',
         "'phpmussel-form-target'",
--- src/pages/siginfo.php
+++ src/pages/siginfo.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: The signature information page (last modified: 2025.09.01).
+ * This file: The signature information page (last modified: 2025.10.12).
  */
 
 namespace phpMussel\FrontEnd;
@@ -23,11 +23,139 @@ $this->initialPrepwork($FE, $this->Loader->L10N->getString('link.Signature Infor
 /** Append number localisation JS. */
 $FE['JS'] .= $this->numberJs() . "\n";
 
-$FE['InfoRows'] = '';
 $FE['SigInfoMenuOptions'] = '';
 
 /** Process signature files and fetch relevant values. */
-$this->signatureInformationHandler($FE['InfoRows'], $FE['SigInfoMenuOptions']);
+if (!$this->Loader->loadShorthandData()) {
+    $FE['InfoRows'] = '      <span class="s">' . $this->Loader->L10N->getString('response.Error') . "</span>\n";
+} else {
+    $FE['InfoRows'] = '';
+
+    /** Template for range rows. */
+    $InfoRow = $this->Loader->readFile($this->getAssetPath('_siginfo_row.html'));
+
+    /** Get list of vendor search patterns and metadata search pattern partials. */
+    $Arr = [
+        'Vendors' => $this->Loader->InstanceCache['shorthand.yml']['Vendor Search Patterns'],
+        'SigTypes' => $this->Loader->InstanceCache['shorthand.yml']['Metadata Search Pattern Partials']
+    ];
+
+    /** Expand patterns for signature metadata. */
+    foreach ($Arr['SigTypes'] as &$Arr['Type']) {
+        $Arr['Type'] = sprintf('\x1A(?![\x80-\x8F])[\x0%1$s\x1%1$s\x2%1$s\x3%1$s\x4%1$s\x5%1$s\x6%1$s\x7%1$s\x8%1$s\x9%1$s\xa%1$s\xb%1$s\xc%1$s\xd%1$s\xe%1$s\ef%1$s].', $Arr['Type']);
+    }
+
+    /** Get list of vector search patterns. */
+    $Arr['Targets'] = $this->Loader->InstanceCache['shorthand.yml']['Vector Search Patterns'];
+
+    /** Get list of malware type search patterns. */
+    $Arr['MalwareTypes'] = $this->Loader->InstanceCache['shorthand.yml']['Malware Type Search Patterns'];
+
+    /** To be populated by totals. */
+    $Totals = ['Classes' => [], 'Files' => [], 'Vendors' => [], 'SigTypes' => [], 'Targets' => [], 'MalwareTypes' => []];
+
+    /** Signature file classes. */
+    $Classes = [
+        ['General_Command_Detections', ''],
+        ['Filename', '\n(?!>)'],
+        ['Hash', '\n[\dA-Fa-f]{32,}:\d+:'],
+        ['Standard', '\n(?!>)'],
+        ['Standard_RegEx', '\n(?!>)'],
+        ['Normalised', '\n(?!>)'],
+        ['Normalised_RegEx', '\n(?!>)'],
+        ['HTML', '\n(?!>)'],
+        ['HTML_RegEx', '\n(?!>)'],
+        ['PE_Extended', '\n\$PE\w+:[\dA-Fa-f]{32,}:\d+:'],
+        ['PE_Sectional', '\n\d+:[\dA-Fa-f]{32,}:'],
+        ['Complex_Extended', '\n\$\S+;'],
+        ['URL_Scanner', '\n(?:TLD|(?:DOMAIN|URL)(?:-NOLOOKUP)?|QUERY)\S+:']
+    ];
+
+    /** We cycle through this several times in this closure. */
+    $Subs = ['Classes', 'Files', 'Vendors', 'SigTypes', 'Targets', 'MalwareTypes'];
+
+    /** The currently active signature files. */
+    $Active = array_unique(array_filter(explode(',', $this->Loader->Configuration['signatures']['active']), function ($Item) {
+        return !empty($Item);
+    }));
+
+    /** Iterate through active signature files and append totals. */
+    foreach ($Active as $File) {
+        $File = (strpos($File, ':') === false) ? $File : substr($File, strpos($File, ':') + 1);
+        if ($File === '' || $this->Loader->isReserved($File)) {
+            continue;
+        }
+        $Data = $this->Loader->readFile($this->Loader->SignaturesPath . $File);
+        if (substr($Data, 0, 9) !== 'phpMussel') {
+            continue;
+        }
+        $Class = substr($Data, 9, 1);
+        $Nibbles = strlen($Class) ? $this->Scanner->splitNibble($Class) : [-1, -1];
+        $Class = $Classes[$Nibbles[0]] ?? [];
+        $Totals['Files'][$File] = empty($Class[1]) ? 0 : preg_match_all('/' . $Class[1] . '\S+/', $Data);
+        if (isset($Class[1])) {
+            $Totals['Classes'][$Class[0]] = isset($Totals['Classes'][$Class[0]]) ? $Totals['Classes'][$Class[0]] + $Totals['Files'][$File] : $Totals['Files'][$File];
+        }
+        foreach ($Subs as $Sub) {
+            $Totals[$Sub]['Total'] = isset($Totals[$Sub]['Total']) ? $Totals[$Sub]['Total'] + $Totals['Files'][$File] : $Totals['Files'][$File];
+        }
+        $this->normaliseLinebreaks($Data);
+        if (!empty($Class[1])) {
+            foreach (['Vendors', 'SigTypes', 'Targets', 'MalwareTypes'] as $Sub) {
+                foreach ($Arr[$Sub] as $Key => $Pattern) {
+                    $Counts = preg_match_all('/' . $Class[1] . '(?:' . $Pattern . ')\S+/', $Data);
+                    $Totals[$Sub][$Key] = isset($Totals[$Sub][$Key]) ? $Totals[$Sub][$Key] + $Counts : $Counts;
+                }
+            }
+        }
+    }
+
+    /** Build "other" totals. */
+    foreach ($Subs as $Sub) {
+        $Other = $Totals[$Sub]['Total'] ?? 0;
+        foreach ($Totals[$Sub] as $Key => $SubTotal) {
+            if ($Key === 'Total') {
+                continue;
+            }
+            $Other -= $SubTotal;
+        }
+        $Totals[$Sub]['Other'] = $Other;
+    }
+
+    /** Cleanup. */
+    unset($SubTotal, $Other, $Data, $Nibbles, $Active, $File, $Counts, $Arr);
+
+    /** Process totals. */
+    foreach ($Subs as $Sub) {
+        $Label = $this->Loader->L10N->getString('siginfo_sub_' . $Sub) ?: $Sub;
+        $Class = 'sigtype_' . strtolower($Sub);
+        $FE['SigInfoMenuOptions'] .= "\n          <option value=\"" . $Class . '">' . $Label . '</option>';
+        $FE['InfoRows'] .= '      <div class="center h2f s flexstretch ' . $Class . '" style="display:none">' . $Label . "</div>\n      <div class=\"duo flexstretch " . $Class . "_grid\" style=\"display:none\">\n";
+        arsort($Totals[$Sub]);
+        foreach ($Totals[$Sub] as $Key => &$Total) {
+            if (!$Total) {
+                continue;
+            }
+            $Total = $this->NumberFormatter->format($Total);
+            $Label = $this->Loader->L10N->getString(
+                ($Key === 'Other' && $Sub === 'SigTypes') ? 'siginfo_key_Other_Metadata' : 'siginfo_key_' . $Key
+            );
+            if ($Key !== 'Total' && $Key !== 'Other') {
+                if (!$Label) {
+                    $Label = sprintf($this->Loader->L10N->getString('siginfo_xkey'), $Key);
+                }
+                $CellClass = 'h1';
+            } else {
+                $CellClass = 'r';
+            }
+            $FE['InfoRows'] .= $this->Loader->parse(['CellClass' => $CellClass, 'InfoType' => $Label, 'InfoNum' => $Total], $InfoRow);
+        }
+        $FE['InfoRows'] .= "      </div>\n";
+    }
+
+    /** Cleanup. */
+    unset($CellClass, $Key, $Label, $Class, $Sub, $Subs);
+}
 
 /** Calculate and append page load time, and append totals. */
 $FE['ProcTime'] = microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'];
